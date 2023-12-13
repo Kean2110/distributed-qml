7057c42d64f1714dbe6909e29353dd63da19a2a7
Kean2110 <kian.izadi@gmail.com>
2023-12-13 15:58:28 +0100

    added circuit layouts

---
+++ qiskit_circuit.py
@@ -0,0 +1,25 @@
+from qiskit import QuantumCircuit
+from qiskit.circuit import Parameter
+from qiskit.circuit.library import RealAmplitudes, ZZFeatureMap
+from qiskit_algorithms.utils import algorithm_globals
+from qiskit_machine_learning.neural_networks import SamplerQNN, EstimatorQNN
+from qiskit_machine_learning.connectors import TorchConnector
+
+def create_qnn():
+    feature_map = ZZFeatureMap(2)
+    ansatz = RealAmplitudes(2, reps=1)
+    qc = QuantumCircuit(2)
+    #qc.compose(feature_map, inplace=True)
+    qc.compose(ansatz, inplace=True)
+    print(qc.decompose().draw())
+    # REMEMBER TO SET input_gradients=True FOR ENABLING HYBRID GRADIENT BACKPROP
+    qnn = EstimatorQNN(
+        circuit=qc,
+        input_params=feature_map.parameters,
+        weight_params=ansatz.parameters,
+        input_gradients=True,
+    )
+    return qnn
+
+qnn = create_qnn()
+print(qnn)
--- quantum_circuit.py
+++ quantum_circuit.py
@@ -1,15 +1,21 @@
 from netqasm.sdk import Qubit
+from netqasm.sdk.external import NetQASMConnection
 from preprocessing import preprocessing_main
 import numpy as np
 
 def encode_in_circuit(image: np.ndarray):
-    values = np.ndarray.flatten(image)
-    print(values)
-    circuit = [Qubit] * len(values)
-    print(circuit)
-    for i, qbit in enumerate(circuit):
-        if values[i]:
-            qbit.X()
+    flattened_image = np.ndarray.flatten(image)
+    print(flattened_image)
+    conn = NetQASMConnection(app_name = "encoding")
+    circuit = []
+    with conn:
+        outcomes = conn.new_array(len(flattened_image))
+        values = conn.new_array(len(flattened_image), init_values=flattened_image)
+        circuit = [Qubit(conn)] * len(values)
+        with values.foreach() as v:
+            q = Qubit(conn=conn)
+            with v.if_eq(1):
+                q.X()
     return circuit
     '''
     qubits = cirq.GridQubit.rect(4, 4)
@@ -23,6 +29,7 @@ def encode_in_circuit(image: np.ndarray):
 def build_encoding():
     (x_train, y_train), (x_test, y_test) = preprocessing_main()
     circ = encode_in_circuit(x_train[0])
+    print(circ)
     #for img in x_train:
     #    encode_in_circuit(img)
         
