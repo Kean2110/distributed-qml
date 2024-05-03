99f551b0b65dc9f39dce5bf9eb926b2e86a19f4f
Kian <kiani@liparit.cip.ifi.lmu.de>
2024-05-03 18:16:24 +0200

    Made output directory for slurm and modified job script

--- job.sh
+++ job.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=dqml
-#SBATCH --mail-user=K.Izadi@campus.lmu.de
-#SBATCH --mail-type=FAIL
-#SBATCH --requeue
+#SBATCH --job-name dqml
+#SBATCH -D ./
+#SBATCH -o ./slurm_output/output.%A.out
+#SBATCH --partition All
+#SBATCH --mail-user K.Izadi@campus.lmu.de
+#SBATCH --mail-type ALL
 echo Running on node $SLURMD_NODENAME at `date`
-. ~/env/bin/activate
+. ./env/bin/activate
 cd two_feature_app
 python main.py 
 echo Finished at `date`
