51d90dfd904eb3a53a5bbb940547dc9c34c17ad3
Kean2110 <kian.izadi@gmail.com>
2024-05-08 13:43:36 +0200

    output folder as arg

--- two_feature_app/utils/model_saver.py
+++ two_feature_app/utils/model_saver.py
@@ -8,11 +8,13 @@ import numpy as np
 
 
 class ModelSaver:
-    def __init__(self) -> None:
+    def __init__(self, output_dir) -> None:
         self.best_loss = None
         self.best_params = None
-        self.file_name = f"parameters/checkpoint_{time.strftime('%Y%m%dT%H%M%S')}.pickle"
-
+        self.file_name = f"checkpoint_{time.strftime('%Y%m%dT%H%M%S')}.pickle"
+        # set and create output dir
+        self.output_dir = os.path.join(output_dir, "parameters/")
+        os.mkdir(self.output_dir)
     
     def save_intermediate_results(self, params, loss):
         if self.best_loss is not None and loss >= self.best_loss:
@@ -20,8 +22,7 @@ class ModelSaver:
         else:
             self.best_loss = loss
             self.best_params = params
-            parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),".."))
-            file_location = os.path.join(parent_dir, self.file_name)
+            file_location = os.path.join(self.output_dir, self.file_name)
             intermediate_dict = {'params': self.best_params, 'loss': self.best_loss}
             logger.info("Saving intermediate results to pickle file.")
             with open(file_location, 'wb') as file:   
