42a482be9863a9f0062223dd8ac7b37c1ff09381
Kean2110 <kian.izadi@gmail.com>
2024-05-21 20:18:23 +0200

    fixing output path

--- job.sh
+++ job.sh
@@ -9,5 +9,5 @@
 echo Running on node $SLURMD_NODENAME at `date`
 . ./env/bin/activate
 cd two_feature_app
-python main.py $SLURM_ARRAY_TASK_ID $SLURM_JOB_ID$SLURM_ARRAY_TASK_ID
+python main.py $SLURM_ARRAY_TASK_ID $SLURM_ARRAY_JOB_ID$SLURM_ARRAY_TASK_ID
 echo Finished at `date`
