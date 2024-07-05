b42a3851feb1f2b3cee2821831eaf74cd7023196
Kean2110 <kian.izadi@gmail.com>
2024-07-05 12:46:47 +0200

    looping qubit ladder now, and creating EPR pairs after Qubits (bugfixed)

--- qubit_ladder/app_node1.py
+++ qubit_ladder/app_node1.py
@@ -1,12 +1,16 @@
+import os
+#os.environ["NETQASM_SIMULATOR"] = "netsquid_single_thread"
 from netqasm.runtime.settings import Simulator, get_simulator
 from netqasm.sdk import EPRSocket, Qubit
 from netqasm.sdk.external import NetQASMConnection, Socket, get_qubit_state
 from netqasm.sdk.toolbox.sim_states import get_fidelity, qubit_from, to_dm
 from teleport_qubit import remote_cnot_control
+from netqasm.logging.glob import get_netqasm_logger
+
 
 def main(app_config=None):
+    logger = get_netqasm_logger()
     log_config = app_config.log_config
-
     # Create a socket to communicate classical information
     socket = Socket("node1", "node2", log_config=log_config)
 
@@ -17,22 +21,25 @@ def main(app_config=None):
     node1 = NetQASMConnection(
         app_name="node1", log_config=log_config, epr_sockets=[epr_socket]
     )
-
     with node1:
-        q0 = Qubit(node1)
-        q1 = Qubit(node1)
-        q0.X()
-        q0.cnot(q1)
-        
-        remote_cnot_control(epr_socket, socket, node1, q1)
-
-        node1.flush()
-        q0_state = get_qubit_state(q0)
-        q1_state = get_qubit_state(q1)
-        print(
-            f"Qubit state q0: {q0_state.tolist()}, Qubit state q1: {q1_state.tolist()}"
-        )
-        return {"q0_state": q0_state.tolist(), "q1_state": q1_state.tolist()}
+        for i in range(500):
+            logger.info(f"Enter iter {i}")
+            q0 = Qubit(node1)
+            q1 = Qubit(node1)
+            q0.X()
+            q0.cnot(q1)
+            node1.flush()
+            remote_cnot_control(epr_socket, socket, node1, q1)
+            
+            q0_state = get_qubit_state(q0)
+            q1_state = get_qubit_state(q1)
+            print(
+                f"Qubit state q0: {q0_state.tolist()}, Qubit state q1: {q1_state.tolist()}"
+            )
+            q0_res = q0.measure()
+            q1_res = q1.measure()
+            node1.flush()
+            print(f"Measurement results; Q0 : {q0_res}, Q1: {q1_res}")
 
 
 if __name__ == "__main__":
--- qubit_ladder/app_node2.py
+++ qubit_ladder/app_node2.py
@@ -1,13 +1,17 @@
+import os
+#os.environ["NETQASM_SIMULATOR"] = "netsquid_single_thread"
 from netqasm.logging.output import get_new_app_logger
 from netqasm.runtime.settings import Simulator, get_simulator
 from netqasm.sdk import EPRSocket, Qubit
 from netqasm.sdk.classical_communication.message import StructuredMessage
 from netqasm.sdk.external import NetQASMConnection, Socket, get_qubit_state
+from netqasm.logging.glob import get_netqasm_logger
 from netqasm.sdk.toolbox import set_qubit_state
 from teleport_qubit import remote_cnot_target
 
 def main(app_config=None, phi=0.0, theta=0.0):
-    log_config = app_config.log_config
+    logger = get_netqasm_logger()
+    log_config = app_config.log_confog
     # Create a socket to send classical information
     socket = Socket("node2", "node1", log_config=log_config)
 
@@ -19,25 +23,29 @@ def main(app_config=None, phi=0.0, theta=0.0):
         app_name="node2", log_config=log_config, epr_sockets=[epr_socket]
     )
     with node2:
-        # target qubit
-        q2 = Qubit(node2)
-        q2.X()
-
-        remote_cnot_target(epr_socket, socket, node2, q2)
-        
-        # build ladder
-        q3 = Qubit(node2)
-        q3.X()
-        q2.cnot(q3)
-        node2.flush()
-        
-        
-        q2_state = get_qubit_state(q2)
-        q3_state = get_qubit_state(q3)
-        print(
-            f"Qubit state q2: {q2_state.tolist()}, Qubit state q3: {q3_state.tolist()}"
-        )
-        return {"q2_state": q2_state.tolist(), "q3_state": q3_state.tolist()}
+        for i in range(500):
+            logger.info(f"Enter iter {i}")
+            # target qubit
+            q2 = Qubit(node2)
+            q3 = Qubit(node2)
+            q2.X()
+            node2.flush()
+            remote_cnot_target(epr_socket, socket, node2, q2)
+            # build ladder
+            q3.X()
+            q2.cnot(q3)
+            node2.flush()
+            
+            q2_state = get_qubit_state(q2)
+            q3_state = get_qubit_state(q3)
+            print(
+                f"Qubit state q2: {q2_state.tolist()}, Qubit state q3: {q3_state.tolist()}"
+            )
+            
+            q2_res = q2.measure()
+            q3_res = q3.measure()
+            node2.flush()
+            print(f"Measurement results; Q2 : {q2_res}, Q3: {q3_res}")
 
 
 if __name__ == "__main__":
