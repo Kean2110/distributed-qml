0a2fdd9023079441e0425ff1320e5b45bcb8d732
Kean2110 <kian.izadi@gmail.com>
2024-10-30 15:56:27 +0100

    bugfix for default value of layers_with_rcnot

--- utils/config_parser.py
+++ utils/config_parser.py
@@ -24,7 +24,7 @@ class ConfigParser:
     dataset_function = "MOONS"
     start_from_checkpoint = False
     n_qubits = 2
-    layers_with_rcnot = list(range(q_depth)) # per default all layers have a RCNOT
+    layers_with_rcnot = None
 
     def __new__(cls, config_path=None, run_id=None):
         if cls._instance is None:
@@ -53,6 +53,8 @@ class ConfigParser:
         for key, value in self.config.items():
             ConfigParser.check_restrictions(key, value)
             setattr(self, key, value)
+        if not self.layers_with_rcnot:
+            self.layers_with_rcnot = list(range(self.q_depth)) # per default all layers have a RCNOT
         ConfigParser.check_epr_list_restriction(self.layers_with_rcnot, self.q_depth)
     
     def get_config(self) -> dict:
